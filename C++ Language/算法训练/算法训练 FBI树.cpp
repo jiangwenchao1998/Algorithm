69a8f54d5333d235e5cd2bc05ba5b796b71e032d
jiangwenchao <36568375+jiangwenchao1998@users.noreply.github.com>
2019-03-02 16:25:17 +0800

    Update 算法训练 FBI树.cpp

--- C++ Language/算法训练/算法训练 FBI树.cpp
+++ C++ Language/算法训练/算法训练 FBI树.cpp
@@ -16,10 +16,13 @@ string FBI(string str) {
         }
     }
     //二叉树后续遍历
+	//左子树
     string strA = str.substr(0, str.length()/2);
     string left = FBI(strA);
+	//右子树
     string strB = str.substr(str.length()/2, str.length());
     string right = FBI(strB);
+	//节点
     if (left.compare(right) == 0) {
         cout << left;
         return left;
